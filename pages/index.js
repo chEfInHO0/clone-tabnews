9c0642e04a494d0281905c97d206dc0cfc7a63c0
Luccas Santos <luccaselias0@gmail.com>
2024-11-27 20:20:02 -0300

    adicionando arquivos modificados pelo prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Um dia de cada vez, melhor do que ontem, pior do que amanhã</h1>
+function Home() {
+  return <h1>Um dia de cada vez, melhor do que ontem, pior do que amanhã</h1>;
 }
 
 export default Home;
