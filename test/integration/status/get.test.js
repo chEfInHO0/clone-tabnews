e69a876e2ff2a9f2f88a87c8493c30f2ce287b22
Luccas Santos <luccaselias0@gmail.com>
2024-11-30 08:55:20 -0300

    criacao dos arquivos `database` e `.env`, exclusao dos antigos arquivos de test e separacao dos teste em `integration` e `unity`, e atualizacao no arquivo `compose` do `docker`

--- test/integration/status/get.test.js
+++ test/integration/status/get.test.js
@@ -1,6 +1,6 @@
 const api_url = 'http://localhost:3000/api/v1/status'
 
-test('', async () => {
+test('Get endpoint /status should return 200', async () => {
   const req = await fetch(api_url)
   expect(req.status).toBe(200)
 })
